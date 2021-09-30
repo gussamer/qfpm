d126f32d7f19d93a9209073c774e457eb79ea572
Angus Alves <angustalves@gmail.com>
2021-09-29 20:48:30 -0400

    fixed script paths again

--- src/build.sh
+++ src/build.sh
@@ -5,5 +5,5 @@ if [ "$DXPM_CURRENT_PACKAGE_NAME" = "" ]; then
   DXPM_CURRENT_PACKAGE_NAME="$(node $PWD/bin/qfpm/getprojectname.js)"
 fi
 echo "[+] converting source for $DXPM_CURRENT_PACKAGE_NAME"
-sfdx force:source:convert -d "./manifest" -n "$DXPM_CURRENT_PACKAGE_NAME" -p "$(node $PWD/bin/qfpm/srcfolders.js)" && wait
+sfdx force:source:convert -d "./manifest" -n "$DXPM_CURRENT_PACKAGE_NAME" -p "$(node $PWD/bin/qfpm/getsrcfolders.js)" && wait
 echo "[+] finished converting source for $DXPM_CURRENT_PACKAGE_NAME"
